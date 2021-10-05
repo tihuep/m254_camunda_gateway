390150416e9719625f070dafaefa3c56ae3c3eae
unknown <timon.hueppi@gmail.com>
2021-10-05 11:24:54 +0200

    Initial commit

---
+++ src/main/java/ch/timonhueppi/tbz/camunda_gateway/CamundaGatewayApplication.java
@@ -0,0 +1,13 @@
+package ch.timonhueppi.tbz.camunda_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/timonhueppi/tbz/camunda_gateway/CamundaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.timonhueppi.tbz.camunda_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
